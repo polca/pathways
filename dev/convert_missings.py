25d5020aca08d902602d89521f8ae686ebc19a34
romainsacchi <r_s@me.com>
2024-02-22 12:14:34 +0000

    Black reformating

--- dev/convert_missings.py
+++ dev/convert_missings.py
@@ -2,16 +2,20 @@ import pandas as pd
 import yaml
 
 # Load the CSV file
-file_path = 'missing_classifications.csv'
+file_path = "missing_classifications.csv"
 data = pd.read_csv(file_path)
 
 # Convert dataframe to a list of tuples, matching the desired output structure
-data_tuples = [{tuple(x): "Other transportation support activities"} for x in data.to_numpy()]
+data_tuples = [
+    {tuple(x): "Other transportation support activities"} for x in data.to_numpy()
+]
 
 # Prepare the data in the specified YAML format with the custom tag
-yaml_data = yaml.dump(data_tuples, default_flow_style=False, allow_unicode=True, sort_keys=False)
+yaml_data = yaml.dump(
+    data_tuples, default_flow_style=False, allow_unicode=True, sort_keys=False
+)
 
 # Save the YAML data to a file
-yaml_file_path = 'converted_yaml.yaml'
-with open(yaml_file_path, 'w') as file:
+yaml_file_path = "converted_yaml.yaml"
+with open(yaml_file_path, "w") as file:
     file.write(yaml_data)
