b2b2ab9cdb0f75b7a1bf7e2628470a27fd7842ea
romainsacchi <r_s@me.com>
2024-03-11 10:59:05 +0000

    Black reformating

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "scipy",
         "premise",
         "pyyaml",
-        "scikit-umfpack"
+        "scikit-umfpack",
     ],
     url="https://github.com/polca/premise",
     description="Scenario-level LCA of energy systems and transition pathways",
