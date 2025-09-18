8fce8c114cbd3f1fc8151eeb4a5e12cd180a7413
romainsacchi <r_s@me.com>
2025-09-18 16:54:55 +0200

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,14 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"
 templates_path = ["_templates"]
 exclude_patterns = []
 
+# docs/conf.py
+autodoc_mock_imports = [
+    "numpy", "pandas", "bw2calc", "bw2data", "sparse", "xarray",
+    "prettytable", "tqdm", "constructive_geometries"
+]
+
+
+
 html_theme = "alabaster"
 html_static_path = ["_static"]
 html_logo = "https://raw.githubusercontent.com/polca/pathways/refs/heads/main/assets/pathways-high-resolution-logo-transparent.png"
